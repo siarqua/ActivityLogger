218c43ec79faf5a24e85be7b0d60e606837ad681
siarqua <siarqua@o2.pl>
2015-09-01 19:11:20 +0200

    add transactions to dao

--- src/main/java/org/some/pkg/dao/ActivityDAO.java
+++ src/main/java/org/some/pkg/dao/ActivityDAO.java
@@ -3,6 +3,7 @@ package org.some.pkg.dao;
 import org.some.pkg.entity.Activity;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -10,6 +11,7 @@ import java.util.List;
  * Created by siarqua on 10.08.15.
  */
 @Repository
+@Transactional
 public interface ActivityDAO extends PagingAndSortingRepository<Activity, Long> {
 
     List<Activity> findByUserId(Long id);
--- src/main/java/org/some/pkg/dao/DetailDAO.java
+++ src/main/java/org/some/pkg/dao/DetailDAO.java
@@ -3,11 +3,13 @@ package org.some.pkg.dao;
 import org.some.pkg.entity.Detail;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by siarqua on 10.08.15.
  */
 @Repository
+@Transactional
 public interface DetailDAO extends PagingAndSortingRepository<Detail,Long> {
 
     Detail findById(Long id);
